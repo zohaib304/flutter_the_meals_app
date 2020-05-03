739fa487703efbc3f1316361a1e7525885dbc887
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-03 16:21:46 +0500

    a

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/category_item.dart';
 
+import 'category_item.dart';
 import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
