e6a601d3787ebbc30e15e355904e442413c3b2a6
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-07 15:22:15 +0500

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import './screens/tabs_screen.dart';
 import './screens/meal_details_screen.dart';
 import './screens/category_meals_screen.dart';
-import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
