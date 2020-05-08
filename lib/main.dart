ac92983f48afe37794b4dbce0f71b77d47822e59
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-08 17:29:15 +0500

    Adding Links to the Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/meal_details_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx)  => FiltersScreen(),
       },
     );
   }
