db1f9d5a907c671e99989b90c1c188f55430ae48
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-08 17:17:16 +0500

    Adding a Custom Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
 import './favorties_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -34,6 +35,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 22,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotCondensed',
+          fontSize: 20,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).primaryColor,
+            child: Text(
+              'Cooking',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Colors.white),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals',Icons.restaurant),
+          buildListTile('Filters',Icons.settings),
+        ],
+      ),
+    );
+  }
+}
