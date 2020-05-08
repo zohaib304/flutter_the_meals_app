8f29a169354674ae4e90eef41894d48517f47492
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-08 17:37:40 +0500

    Replacing Pages (Instead of Pusing)

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+import '../widgets/main_drawer.dart';
 
+class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("filter"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text("filter"),
+      ),
     );
   }
 }
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -46,14 +46,14 @@ class MainDrawer extends StatelessWidget {
             'Meals',
             Icons.restaurant,
             () {
-              Navigator.of(context).pushNamed('/');
+              Navigator.of(context).pushReplacementNamed('/');
             },
           ),
           buildListTile(
             'Filters',
             Icons.settings,
             () {
-              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+              Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
             },
           ),
         ],
