a8b5433c6445a3eb4be1262effde58e6a2293407
zohaib304 <muhammad.zohaib9613@hotmail.com>
2020-05-11 16:17:09 +0500

    Popping Pages & Passing Data Back

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -2,38 +2,65 @@ import 'package:flutter/material.dart';
 import '../widgets/meal_item.dart';
 
 import '../dummy_data.dart';
+import '../models/meal.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
-  // final String categoryId;
-  // final String categoryTitle;
 
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayMeals;
+  var _loadedInitData = false;
+
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      final categoryId = routeArgs['id'];
+      categoryTitle = routeArgs['title'];
+
+      displayMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void initState() {
+    // ...
+    super.initState();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryId = routeArgs['id'];
-    final categoryTitle = routeArgs['title'];
-
-    final categoryMeal = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
           return MealItem(
-            id: categoryMeal[index].id,
-            title: categoryMeal[index].title,
-            duraction: categoryMeal[index].duration,
-            complexity: categoryMeal[index].complexity,
-            imageURL: categoryMeal[index].imageUrl,
-            affordability: categoryMeal[index].affordability,
+            id: displayMeals[index].id,
+            title: displayMeals[index].title,
+            duraction: displayMeals[index].duration,
+            complexity: displayMeals[index].complexity,
+            imageURL: displayMeals[index].imageUrl,
+            affordability: displayMeals[index].affordability,
+            removeItem: _removeMeal,
           );
         },
-        itemCount: categoryMeal.length,
+        itemCount: displayMeals.length,
       ),
     );
   }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,6 +10,7 @@ class MealItem extends StatelessWidget {
   final int duraction;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem({
     @required this.id,
@@ -18,6 +19,7 @@ class MealItem extends StatelessWidget {
     @required this.complexity,
     @required this.duraction,
     @required this.imageURL,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -57,7 +59,11 @@ class MealItem extends StatelessWidget {
     Navigator.of(context).pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
-    ); 
+    ).then((result) {
+      if(result != null) {
+        removeItem(result);
+      }
+    }); 
   }
 
   @override
